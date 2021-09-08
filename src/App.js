f5ac03a12ef975cc8d1afdf49d2bad6a99811b65
Guilherme Ciole <guilhermeciole@gmail.com>
2021-09-08 18:17:03 -0300

    feat: post creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
-import GlobalStyle from './global';
+import "./App.css";
+import GlobalStyle from "./global";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle>
-        <h1>Teste</h1>
-      </GlobalStyle>
+      <GlobalStyle><h1>Teste</h1></GlobalStyle>
     </div>
   );
 }
