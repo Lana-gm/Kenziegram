38a85714102a9b9f7d13e467b750e2404cd475b8
Guilherme Ciole <guilhermeciole@gmail.com>
2021-09-09 10:52:26 -0300

    fix: update page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,31 +3,26 @@ import Landing from "../pages/Landing";
 import Login from "../pages/Login";
 import ProfilePage from "../pages/Profile";
 const Routes = () => {
-    return (
-        <Switch>
-            <Route exact path="/">
-                <Landing/>
-            </Route>
-            <Route path="/register">
-            </Route>
-            <Route path="/home">
-
-            </Route>
-            <Route path="/login">
-                <Login />
-            </Route>
-            <Route path="/profile">
-                <ProfilePage />
-            </Route>
-            <Route path="/profile/:id">
-
-            </Route>
-            <Route path="/profile/edit">
-
-            </Route>
-
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Landing />
+      </Route>
+      <Route path="/register"></Route>
+      <Route path="/home"></Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/search">
+        
+      </Route>
+      <Route path="/profile">
+          <ProfilePage />
+      </Route>
+      <Route path="/profile/:id"></Route>
+      <Route path="/profile/edit"></Route>
+    </Switch>
+  );
 };
 
 export default Routes;
