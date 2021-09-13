c506e9630252b680ccb53e9c7dfc6e82c3103c0c
Guilherme Ciole <guilhermeciole@gmail.com>
2021-09-13 10:14:03 -0300

    fix: updated the scroll of the stories

--- src/components/Storys/style.js
+++ src/components/Storys/style.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   height: 188px;
   width: 100vw;
   background-color: var(--bgLight);
-  padding: 1rem 1rem 1rem;
+  padding: 1rem 0rem 1rem;
   position: relative;
 
   .titulos {
@@ -16,6 +16,7 @@ export const Container = styled.div`
     color: var(--white);
     font-size: 30px;
     font-weight: bold;
+    margin-left: 1rem;
   }
 
   .amigos {
@@ -23,23 +24,29 @@ export const Container = styled.div`
     font-weight: bold;
     font-size: 18px;
     padding-top: 0.5rem;
+    margin-right: 1rem;
   }
 
-  .usuarios {
+  #usuariosId {
     display: flex;
     margin-top: 1rem;
     overflow-x: auto;
     overflow-y: hidden;
+    scroll-behavior: smooth;
+    ::-webkit-scrollbar {
+      display: none;
+    }
   }
 
   .usuario {
     margin-right: 0.5rem;
+    margin-left: 0.5rem;
     min-width: 4.5rem;
     max-height: 6rem;
   }
 
   .nome {
-      color: var(--white);
+    color: var(--white);
   }
 
   .imagem {
@@ -50,23 +57,29 @@ export const Container = styled.div`
     border-image: linear-gradient(135deg, red 0%, blue 100%) 1;
   }
 
+  .x {
+    color: white;
+    z-index: 100;
+  }
+
   @media (min-width: 1000px) {
     width: 975px;
     margin-top: 5rem;
-
-    .usuarios {
-      scroll-behavior: smooth;
-      ::-webkit-scrollbar {
-        display: none;
-      }
-    }
 
     #back {
       color: var(--blueIcons);
       cursor: pointer;
       font-size: 30px;
       position: absolute;
-      top: 6rem;
+      top: 5rem;
+      left: 1rem;
+      border-radius: 100%;
+      box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+        rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+        rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset,
+        rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px,
+        rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px,
+        rgba(0, 0, 0, 0.09) 0px 32px 16px;
     }
 
     #next {
@@ -75,8 +88,15 @@ export const Container = styled.div`
       cursor: pointer;
       font-size: 30px;
       position: absolute;
-      top: 6rem;
+      top: 5rem;
       right: 1rem;
+      border-radius: 100%;
+      box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+        rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+        rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset,
+        rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px,
+        rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px,
+        rgba(0, 0, 0, 0.09) 0px 32px 16px;
     }
   }
 `;
